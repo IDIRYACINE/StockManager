36ee428910ef7800518eba870d881e2cfa694b2d
IDIRYACINE <idiryacinesp@gmail.com>
2023-02-23 14:44:33 +0000

    feature : prisma database setup

---
+++ server_app/src/index.ts
@@ -0,0 +1,11 @@
+
+import express from "express";
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+
+
+app.listen(port, () => {
+    console.log("Server is running on port 3000")
+});
